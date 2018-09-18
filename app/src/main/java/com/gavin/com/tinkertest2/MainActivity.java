27189cb5a131cf85ecf0e0d30b07f021a999940d
zjl7577 <1097919195@qq.com>
2018-09-18 10:05:10 +0800

    before patch

--- app/src/main/java/com/gavin/com/tinkertest2/MainActivity.java
+++ app/src/main/java/com/gavin/com/tinkertest2/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "I`m patch! version 4  no internet", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "I`m patch! version 1  no internet", Toast.LENGTH_SHORT).show();
     }
 }
